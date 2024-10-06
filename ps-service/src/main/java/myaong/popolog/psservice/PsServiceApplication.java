542baf87f3db0ce03304f12578e3a981b6fd7290
strongmhk <kim6562166086@gmail.com>
2024-10-06 23:52:07 +0900

    :wrench: chore: eureka client 기본 세팅

--- ps-service/src/main/java/myaong/popolog/psservice/PsServiceApplication.java
+++ ps-service/src/main/java/myaong/popolog/psservice/PsServiceApplication.java
@@ -2,8 +2,10 @@ package myaong.popolog.psservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PsServiceApplication {
 
     public static void main(String[] args) {
