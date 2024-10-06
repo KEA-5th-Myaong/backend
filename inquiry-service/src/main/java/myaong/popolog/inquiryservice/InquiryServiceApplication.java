70bd043534612d5d385fa3b0235e8c01a484e421
strongmhk <kim6562166086@gmail.com>
2024-10-07 00:06:14 +0900

    init: 문의 서비스 프로젝트 생성

---
+++ inquiry-service/src/main/java/myaong/popolog/inquiryservice/InquiryServiceApplication.java
@@ -0,0 +1,13 @@
+package myaong.popolog.inquiryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InquiryServiceApplication.class, args);
+    }
+
+}
