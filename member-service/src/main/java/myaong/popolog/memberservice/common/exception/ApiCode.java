243ef5bc78cc142a6463a096eb4c4f9aab4d1458
Sujin Kang <1214sujin@naver.com>
2024-10-14 12:34:15 +0900

    ✨ [#5] feature: API 공통 응답, Custom Exception 정의, Exception Handler 구현

---
+++ member-service/src/main/java/myaong/popolog/memberservice/common/exception/ApiCode.java
@@ -0,0 +1,24 @@
+package myaong.popolog.memberservice.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ApiCode {
+
+	OK(HttpStatus.OK, "COMMON_2000", "OK"),
+	INVALID_DATA(HttpStatus.BAD_REQUEST, "COMMON_4000", "Request data missing or invalid"),
+	TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "COMMON_4010", "Token invalid"),
+	TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "COMMON_4011", "Token expired"),
+	FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON_4030", "Forbidden"),
+	READ_ONLY_ACCESS(HttpStatus.FORBIDDEN, "COMMON_4031", "You can only read"),
+	METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "COMMON_4050", "Method not allowed"),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_5000", "Internal Server Error"),
+	DB_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_5001", "DB Error");
+
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String message;
+}
